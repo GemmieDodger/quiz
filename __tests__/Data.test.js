b2db914ae9c0d0177cb5acd69438beffc37710ed
gemmiedodger <gemmadruce@live.co.uk>
2021-12-04 12:49:27 +0000

    add test to check can reach data

---
+++ __tests__/Data.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import firebase from "../src/Firebase";
+
+test('can reach quiz data from firebase', () => {
+    const onCollectionUpdate = (querySnapshot) => {
+        const quizzes = []
+        expect(quizzes.length).toBe(0)
+        querySnapshot.forEach((doc) => {
+          const { quizName } = doc.data();
+          quizzes.push({
+            key: doc.id,
+            doc, // DocumentSnapshot
+            quizName,
+          });
+        });
+        expect(internalQuizzes.length).toBeGreaterThan(0)
+      };
+
+    const ref = firebase.firestore().collection("quizzes");
+    ref.get().then((doc) => {
+      if (!doc.exists) {
+        console.log("No such document!");
+      }
+    });
+    ref.onSnapshot(onCollectionUpdate);
+});
--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,8 @@ import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-//THIS IS NOT LI
-
-test('renders learn react link', () => {
+test('renders expected text in render of App', () => {
   render(<App />);
-  screen.debug();
   const linkElement = screen.getByText(/You've made it to the QUESTIONSHOP!/i);
   expect(linkElement).toHaveTextContent;
 });
