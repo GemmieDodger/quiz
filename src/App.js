393d6bef9ac21f90f672ce154b7350037c5c517f
gemmiedodger <gemmadruce@live.co.uk>
2021-12-04 13:28:55 +0000

    tests are working if you hide the import for bootstrap. This does need a solve in a future commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 import 'bootstrap';
